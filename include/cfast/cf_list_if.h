789b61cd251c60f2730c8d4a2d2e8c3929b3602b
tkorays <tkorays@hotmail.com>
2018-01-04 23:51:26 +0800

    complete double list

--- include/cfast/cf_list_if.h
+++ include/cfast/cf_list_if.h
@@ -29,7 +29,7 @@ typedef cf_void_t* cf_list_iter_t;
 
 cf_list_t*  cf_list_new(fn_cf_list_free func);
 cf_ret_t    cf_list_insert(cf_list_t* li, cf_void_t* data, cf_int32_t pos);
-cf_ret_t    cf_list_remove(cf_list_t* li, cf_void_t* data, cf_int32_t pos);
+cf_ret_t    cf_list_remove(cf_list_t* li, cf_void_t** data, cf_int32_t pos);
 cf_ret_t    cf_list_delete(cf_list_t* li, cf_int32_t pos, cf_bool_t free_data);
 cf_ret_t    cf_list_free(cf_list_t* li, cf_bool_t free_data);
 cf_ret_t    cf_list_iter_init(cf_list_t* li, cf_list_iter_t* it);
--- src/list/double_link_list.c
+++ src/list/double_link_list.c
@@ -32,39 +32,6 @@ cf_ret_t cf_list_insert(cf_list_t* li, cf_void_t* data, cf_int32_t pos) {
     node = (cf_list_node_t*)malloc(sizeof(cf_list_node_t));
     node->data = data;
 
-    /* 链表为空，则直接插入 */
-    if(CF_NULL_PTR == li->head) {
-        node->prev = CF_NULL_PTR;
-        node->prev = CF_NULL_PTR;
-        li->head = node;
-        li->tail = node;
-        li->number++;
-        return CF_RET_SUCCESS;
-    }
-
-    /* 插入头部 */
-    if(CF_LIST_POS_HEAD == pos) {
-        tmp = li->head;
-        node->prev = CF_NULL_PTR;
-        node->next = tmp;
-        tmp->prev = node;
-        li->head = node;
-        li->number++;
-        return CF_RET_SUCCESS;
-    } 
-
-    /* 插入到尾部 */
-    if(CF_LIST_POS_TAIL == pos) {
-        tmp = li->tail;
-        node->prev = tmp;
-        node->next = CF_NULL_PTR;
-        tmp->next = node;
-        li->tail = node;
-        li->number++;
-        return CF_RET_SUCCESS;
-    }
-
-    /* 插入到中间，正数从前往后，负数从后往前 */
     abs_pos = (pos >= 0 ? pos : -pos);
     if(abs_pos > li->number) {
         free(node);
@@ -76,62 +43,41 @@ cf_ret_t cf_list_insert(cf_list_t* li, cf_void_t* data, cf_int32_t pos) {
     }
 
     for(index = 0, tmp = li->head; index < abs_pos && tmp; index++, tmp = tmp->next) ;
-    if(!tmp) {
-        free(node);
-        return CF_RET_FAIL;
-    }
-
-    node->prev = tmp;
-    node->next = tmp->next;
-    if(tmp->next) {
+    if(tmp == CF_NULL_PTR) {
+        /* empty */
+        node->prev = CF_NULL_PTR;
+        node->next = CF_NULL_PTR;
+        li->head = node;
+        li->tail = node;
+    }else if(tmp->prev == CF_NULL_PTR) {
+        /* head */
+        node->prev = CF_NULL_PTR;
+        node->next = tmp;
+        tmp->prev = node;
+        li->head = node;
+    } else if(tmp->next == CF_NULL_PTR) {
+        /* tail */
+        node->prev = tmp;
+        node->next = CF_NULL_PTR;
+        tmp->next = node;
+        li->tail = node;
+    } else {
+        node->prev = tmp;
+        node->next = tmp->next;
         tmp->next->prev = node;
+        tmp->next = node;
     }
-    tmp->next = node;
     li->number++;
 
     return CF_RET_SUCCESS;
 }
 
-cf_ret_t    cf_list_remove(cf_list_t* li, cf_void_t* data, cf_int32_t pos) {
+cf_ret_t    cf_list_remove(cf_list_t* li, cf_void_t** data, cf_int32_t pos) {
     cf_list_node_t* node = CF_NULL_PTR;
     cf_list_node_t* tmp = CF_NULL_PTR;
     cf_uint32_t abs_pos = 0;
     cf_uint32_t index;
     if(!li || !data) return CF_RET_NULL_PTR;
-
-    /* 链表为空 */
-    if(li->head == CF_NULL_PTR) {
-        data = CF_NULL_PTR;
-        return CF_RET_FAIL;
-    }
-
-    /* 移除头节点 */
-    if(CF_LIST_POS_HEAD == pos) {
-        node = li->head;
-        li->head = node->next;
-        if(node->next) {
-            node->next->prev = CF_NULL_PTR;
-        } else {
-            li->tail = CF_NULL_PTR;
-        }
-        data = node->data;
-        free(node);
-        return CF_RET_SUCCESS;
-    }
-
-    /* 移除尾部节点 */
-    if(CF_LIST_POS_TAIL == pos) {
-        node = li->tail;
-        li->tail = node->prev;
-        if(node->prev) {
-            node->prev->next = CF_NULL_PTR;
-        } else {
-            li->head = CF_NULL_PTR;
-        }
-        data = node->data;
-        free(node);
-        return CF_RET_SUCCESS;
-    }
 
     abs_pos = (pos >= 0 ? pos : -pos);
     if(abs_pos > li->number) {
@@ -152,14 +98,41 @@ cf_ret_t    cf_list_remove(cf_list_t* li, cf_void_t* data, cf_int32_t pos) {
         } else {
             li->tail = CF_NULL_PTR;
         }
-        data = node->data;
-        free(node);
     } else if(node->next == CF_NULL_PTR) {
         /* tail */
-
+        li->tail = node->prev;
+        if(node->prev) {
+            node->prev->next = CF_NULL_PTR;
+        } else {
+            li->head = CF_NULL_PTR;
+        }
+    } else {
+        tmp = node->prev;
+        tmp->next = node->next;
+        node->next->prev = tmp;
     }
+    li->number--;
+    *data = node->data;
+    free(node);
 
+    return CF_RET_SUCCESS;
 }
+
+cf_ret_t cf_list_delete(cf_list_t* li, cf_int32_t pos, cf_bool_t free_data) {
+    cf_ret_t ret = CF_RET_SUCCESS;
+    cf_void_t* data = CF_NULL_PTR;
+    if(!li) return CF_RET_FAIL;
+    if(!li->fn_free && free_data) return CF_RET_FAIL;
+
+    ret = cf_list_remove(li, &data, pos);
+    if(ret != CF_RET_SUCCESS) {
+        return CF_RET_FAIL;
+    }
+    li->fn_free(data);
+
+    return CF_RET_SUCCESS;
+}
+
 cf_ret_t cf_list_free(cf_list_t* li, cf_bool_t free_data) {
     cf_list_iter_t it = CF_NULL_PTR;
     cf_list_node_t* node = CF_NULL_PTR;
@@ -179,18 +152,18 @@ cf_ret_t cf_list_free(cf_list_t* li, cf_bool_t free_data) {
     return CF_RET_SUCCESS;
 }
 
-cf_ret_t    cf_list_iter_init(cf_list_t* li, cf_list_iter_t* it) {
+cf_ret_t cf_list_iter_init(cf_list_t* li, cf_list_iter_t* it) {
     if(!li || !it) return CF_RET_NULL_PTR;
     *it = (cf_list_iter_t)li->head;
     return CF_RET_SUCCESS;
 }
 
-cf_list_iter_t  cf_list_iter_next(cf_list_iter_t it) {
+cf_list_iter_t cf_list_iter_next(cf_list_iter_t it) {
     if(!it) return CF_NULL_PTR;
     return ((cf_list_node_t*)it)->next;
 }
 
-cf_ret_t    cf_list_iter_data(cf_list_iter_t it, cf_void_t* data) {
+cf_ret_t cf_list_iter_data(cf_list_iter_t it, cf_void_t* data) {
     if(!it || !data) return CF_RET_NULL_PTR;
     data = ((cf_list_node_t*)it)->data;
     return CF_RET_SUCCESS;
