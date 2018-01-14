b6f61ed9bfa19472115fd50374fecafd888c5b4e
tkorays <tkorays@hotmail.com>
2018-01-14 22:37:33 +0800

    modify file interface

--- include/cfast/cf_file_if.h
+++ include/cfast/cf_file_if.h
@@ -30,11 +30,17 @@ typedef struct cf_file_s cf_file_t;
 typedef struct cf_file_dir_s cf_file_dir_t;
 typedef struct cf_file_dirent_s cf_file_dirent_t;
 
+// TODO: permission!
 
 cf_file_t*  cf_file_open(const cf_char_t* filename, const cf_char_t* mode);
 cf_errno_t  cf_file_close(cf_file_t* f);
-cf_size_t   cf_file_write(cf_file_t* f, cf_char_t* buff, cf_size_t size, cf_size_t cnt);
-cf_size_t   cf_file_read(cf_file_t* f, cf_char_t* buff, cf_size_t size, cf_size_t cnt);
+cf_errno_t  cf_file_write(cf_file_t* f, cf_char_t* buff, cf_size_t size, cf_size_t cnt);
+cf_errno_t  cf_file_read(cf_file_t* f, cf_char_t* buff, cf_size_t size, cf_size_t cnt);
+cf_bool_t   cf_file_eof(cf_file_t* f);
+cf_errno_t  cf_file_getc(cf_file_t* f, cf_char_t* c);
+cf_errno_t  cf_file_putc(cf_file_t* f, cf_char_t c);
+cf_errno_t  cf_file_gets(cf_file_t* f, cf_char_t* buff, cf_size_t size);
+cf_errno_t  cf_file_puts(cf_file_t* f, const cf_char_t* buff);
 
 cf_file_dir_t*  cf_file_opendir(const cf_char_t* path);
 cf_errno_t  cf_file_closedir(cf_file_dir_t* dir);
@@ -43,8 +49,11 @@ cf_file_dirent_t* cf_file_readdir(cf_file_dir_t* dir);
 cf_bool_t   cf_file_exist(const cf_char_t* filename);
 cf_errno_t  cf_file_rmdir(const cf_char_t* dirname);
 cf_errno_t  cf_file_remove(const cf_char_t* filename);
-cf_errno_t  cf_file_copyto(const cf_char_t* src, const cf_char_t* dst);
-cf_errno_t  cf_file_move(const cf_char_t* src, const cf_char_t* dst);
+cf_errno_t  cf_file_copy(const cf_char_t* src, const cf_char_t* dst);
+cf_errno_t  cf_file_rename(const cf_char_t* src, const cf_char_t* dst);
+cf_errno_t  cf_file_mkdir(const cf_char_t* path);
+cf_errno_t  cf_file_mkdir_r(const cf_char_t* path);
+cf_errno_t  cf_file_link(const cf_char_t* from_path, const cf_char_t* to_path);
 
 cf_errno_t  cf_file_get_extension(const cf_char_t* path, cf_char_t* buf, cf_size_t size);
 cf_errno_t  cf_file_get_basedir(const cf_char_t* path, cf_char_t* buf, cf_size_t size);
