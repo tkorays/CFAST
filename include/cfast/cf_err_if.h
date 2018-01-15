0dc10375bc7a4bb8c601c0fa17ebfe377a4f64d4
tkorays <tkorays@hotmail.com>
2018-01-16 00:50:16 +0800

    save

---
+++ include/cfast/cf_err_if.h
@@ -0,0 +1,48 @@
+#ifndef __CF_ERR_IF_H__
+#define __CF_ERR_IF_H__
+
+#include "cf_def.h"
+
+/**
+ * Errno Description
+ */
+typedef struct {
+    cf_errno_t          err;    /** errno */
+    const cf_char_t*    desc;   /** description */
+} cf_err_info_t;
+
+#define CF_ERR_DESC_MAX_BUFF        128
+#define CF_ERR_MODULE_MAX_NUM       16
+#define CF_MAKE_ERRNO(m, no) ((m)<<16 | no)
+#define CF_ERR_GET_MODULE(err) (((err)>>16) & 0x0000FFFF)
+#define CF_ERR_GET_MERR(err) ((err) & 0x0000FFFF)
+#define CF_OK           CF_MAKE_ERRNO(CF_ERR_MODULE_COMMON, 0)
+#define CF_NOK          CF_MAKE_ERRNO(CF_ERR_MODULE_COMMON, 1)
+#define CF_EPARAM       CF_MAKE_ERRNO(CF_ERR_MODULE_COMMON, 2)
+#define CF_EMALLOC      CF_MAKE_ERRNO(CF_ERR_MODULE_COMMON, 3)
+#define CF_EFOPEN       CF_MAKE_ERRNO(CF_ERR_MODULE_COMMON, 4)
+#define CF_EFCLOSE      CF_MAKE_ERRNO(CF_ERR_MODULE_COMMON, 5)
+#define CF_EFILE_HANDLE CF_MAKE_ERRNO(CF_ERR_MODULE_COMMON, 6)
+#define CF_EFWRITE      CF_MAKE_ERRNO(CF_ERR_MODULE_COMMON, 7)
+#define CF_EFREAD       CF_MAKE_ERRNO(CF_ERR_MODULE_COMMON, 8)
+
+
+/**
+ * Get description of an error.
+ * @param err           Errno of an error.
+ * @param desc          Description buffer.
+ * @param size          Size of 'desc' buffer.
+ * @return 
+ */
+cf_void_t cf_err_get_info(cf_errno_t err, cf_char_t* desc, cf_size_t size);
+
+/**
+ * Register error info to global config.
+ * @param module        Module number.
+ * @param errs          Errors.
+ * @param size          Size of 'errs'.
+ * @return
+ */
+cf_void_t cf_err_register(cf_uint32_t module, cf_err_info_t* errs, cf_size_t size);
+
+#endif /* __CF_ERR_IF_H__ */
---
+++ src/common/cf_err.c
@@ -0,0 +1,42 @@
+#include "cfast/cf_err_if.h"
+#include "cfast/cf_str_if.h"
+
+typedef struct {
+    cf_size_t   size;
+    const cf_err_info_t* errs;
+} _cf_errors_in_module_t;
+
+static cf_err_info_t g_common_err_desc[] = {
+    {CF_OK, "ok"},
+    {CF_NOK, "error!"},
+    {CF_EPARAM, "Paramenters error!"},
+    {CF_EMALLOC, "Memory allocation error!"},
+    {CF_EFOPEN, "Fail to open file!"},
+    {CF_EFCLOSE, "Fail to close file!"},
+    {CF_EFILE_HANDLE, "File handle is error!"},
+    {CF_EFWRITE, "Fail to write file."},
+    {CF_EFREAD, "Fail to read file."}
+};
+
+_cf_errors_in_module_t g_err_desc[CF_ERR_MODULE_MAX_NUM] = {
+    {CF_ARRAY_SIZE(g_common_err_desc), g_common_err_desc},
+    {0, CF_NULL_PTR}
+};
+
+cf_void_t cf_err_get_info(cf_errno_t err, cf_char_t* desc, cf_size_t size) {
+    cf_uint32_t m = CF_ERR_GET_MODULE(err);
+    cf_uint32_t e = CF_ERR_GET_MERR(err);
+    if(!desc || size == 0) {
+        cf_strcpy_s(desc, size, "Invalid param for cf_err_get_info!");
+        return;
+    }
+    if(m >= CF_ERR_MODULE_MAX_NUM) {
+        cf_strcpy_s(desc, size, "Errno error for cf_err_get_info!");
+        return ;
+    }
+    cf_strcpy_s(desc, size, g_err_desc[m].errs[e].desc);
+}
+
+cf_void_t cf_err_register(cf_uint32_t module, cf_err_info_t* errs, cf_size_t size) {
+
+}
