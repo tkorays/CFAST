ea4bfb517dce09f6993069e198d7c68abdeb3090
tkorays <tkorays@hotmail.com>
2018-01-17 01:40:49 +0800

    add time file

---
+++ include/cfast/cf_time_if.h
@@ -0,0 +1,14 @@
+/****************************************************
+ * Copyright (c) 2018, tkorays <tkorays@hotmail.com>
+ * All rights reserved.
+ * 
+ * @file    cf_time_if.h
+ * @brief   Time.
+ * @version 1.0
+ * @author  tkorays<tkorays@hotmail.com>
+ * @date    2018-1-17
+ ****************************************************/
+#ifndef __CF_TIME_IF_H__
+#define __CF_TIME_IF_H__
+
+#endif /* __CF_TIME_IF_H__ */
