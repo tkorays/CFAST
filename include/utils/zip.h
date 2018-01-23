f892d8a3dc167aaf9425d9ecfbdb955d74a087de
tkorays <tkorays@hotmail.com>
2018-01-23 23:57:40 +0800

    Description : zip Date        : 2018-1-23 Author      : tkorays

---
+++ include/utils/zip.h
@@ -0,0 +1,32 @@
+/****************************************************
+ * Copyright (c) 2018, tkorays <tkorays@hotmail.com>
+ * All rights reserved.
+ * 
+ * @file    zip.h
+ * @brief   Zip.
+ * @version 1.0
+ * @author  tkorays<tkorays@hotmail.com>
+ * @date    2018-1-23
+ ****************************************************/
+#ifndef __CF_UTILS_ZIP_H__
+#define __CF_UTILS_ZIP_H__
+
+#include <cf/types.h>
+
+#define CF_ZIP_CREATE       (0)
+#define CF_ZIP_CREATEAFTER  (1)
+#define CF_ZIP_APPEND       (2)
+
+typedef struct cf_zip_file_s cf_zip_file_t;
+
+cf_zip_file_t* cf_zip_open(const cf_char_t* filename, cf_int_t append);
+cf_errno_t cf_zip_close(cf_zip_file_t* f);
+cf_errno_t cf_zip_open_file_in_zip(cf_zip_file_t* f,
+                        const cf_char_t* filename,
+                        const cf_char_t* comment);
+cf_errno_t cf_zip_close_file_in_zip(cf_zip_file_t* f);
+cf_errno_t cf_zip_write_file_in_zip(cf_zip_file_t* f, const cf_void_t* buff, cf_size_t size);
+
+
+
+#endif /* __CF_UTILS_ZIP_H__ */
--- src/common/opt.c
+++ src/common/opt.c
@@ -5,6 +5,7 @@
 #include <string.h>
  
  cf_errno_t cf_opt_get(cf_uint_t argc, cf_char_t* argv[], const cf_opt_t* optdef, cf_opt_it_t* it) {
+     cf_uint_t i;
      if(argc == 0 || argv == CF_NULL_PTR || optdef == CF_NULL_PTR || it == CF_NULL_PTR) {
          return CF_EPARAM;
      }
