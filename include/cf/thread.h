7419ed22836cd62ae62e86057fb17fcd017be8fd
tkorays <tkorays@hotmail.com>
2018-02-12 22:06:56 +0800

    thread

--- include/cf/thread.h
+++ include/cf/thread.h
@@ -2,8 +2,23 @@
 #define __CF_THREAD_H__
 
 #include <cf/types.h>
+#include <pthread.h>
+
+typedef pthread_t cf_thread_t;
+typedef pthread_attr_t cf_thread_attr_t;
+
+cf_errno_t cf_thread_create(cf_thread_t* t, const cf_thread_attr_t* attr,
+    cf_void_t*(*call_func)(cf_void_t*), cf_void_t* arg);
+cf_errno_t cf_thread_join(cf_thread_t t, void** arg);
+cf_errno_t cf_thread_detach(cf_thread_t t);
+cf_errno_t cf_thread_cancel(cf_thread_t t);
+cf_bool_t cf_thread_equal(cf_thread_t t1, cf_thread_t t2);
+cf_thread_t cf_thread_self();
+cf_errno_t cf_thread_attr_init(cf_thread_attr_t* attr);
+cf_errno_t cf_thread_attr_destroy(cf_thread_attr_t* attr);
 
 cf_uint_t cf_getpid();
 cf_uint_t cf_gettid();
+
 
 #endif /* __CF_THREAD_H__ */
--- src/common/thread.c
+++ src/common/thread.c
@@ -1,10 +1,52 @@
 #include <cf/thread.h>
-#include "pthread.h"
+#include <cf/err.h>
 #ifdef CF_OS_WIN
 #include <process.h>
 #else
 #include <unistd.h>
 #endif
+
+cf_errno_t cf_thread_create(cf_thread_t* t, const cf_thread_attr_t* attr,
+                            cf_void_t*(*call_func)(cf_void_t*), cf_void_t* arg) {
+    if(pthread_create(t, attr, call_func, arg) == 0) return CF_OK;
+    else return CF_NOK;
+}
+
+cf_errno_t cf_thread_join(cf_thread_t t, void** arg) {
+    if(pthread_join(t, arg) == 0) return CF_OK;
+    else return CF_NOK;
+}
+
+cf_errno_t cf_thread_detach(cf_thread_t t) {
+    if(pthread_detach(t) == 0) return CF_OK;
+    else return CF_NOK;
+}
+
+cf_errno_t cf_thread_cancel(cf_thread_t t) {
+    if(pthread_cancel(t) == 0) return CF_OK;
+    else return CF_NOK;
+}
+
+cf_bool_t cf_thread_equal(cf_thread_t t1, cf_thread_t t2) {
+    if(pthread_equal(t1, t2)) return CF_TRUE;
+    else return CF_FALSE;
+}
+
+cf_thread_t cf_thread_self() {
+    return pthread_self();
+}
+
+cf_errno_t cf_thread_attr_init(cf_thread_attr_t* attr) {
+    if(!attr) return CF_EPARAM;
+    if(pthread_attr_init(attr) == 0) return CF_OK;
+    else return CF_NOK;
+}
+
+cf_errno_t cf_thread_attr_destroy(cf_thread_attr_t* attr) {
+    if(!attr) return CF_EPARAM;
+    if(pthread_attr_destroy(attr) == 0) return CF_OK;
+    else return CF_NOK;
+}
 
 cf_uint_t cf_getpid() {
 #ifdef CF_OS_WIN
