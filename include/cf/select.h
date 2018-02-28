b96d0834a79596583e712e6e689b6393a1749fd8
tkorays <tkorays@hotmail.com>
2018-02-28 22:41:23 +0800

    select for linux/windows

--- include/cf/select.h
+++ include/cf/select.h
@@ -1,24 +1,35 @@
+/****************************************************
+ * Copyright (c) 2018, tkorays <tkorays@hotmail.com>
+ * All rights reserved.
+ * 
+ * @file    select.h
+ * @brief   Select for linux/windows.
+ * @version 1.0
+ * @author  tkorays<tkorays@hotmail.com>
+ * @date    2018-2-28
+ ****************************************************/
 #ifndef __CF_SELECT_H__
 #define __CF_SELECT_H__
 
 #include <cf/types.h>
 
-#ifdef CF_OS_WIN
+/**
+ * FD_SETSIZE
+ */
+extern const cf_size_t CF_FD_SETSIZE;
 
-#else
-#include <sys/select.h>
-#endif
-
-#define CF_FD_SET(n, p)     FD_SET(n, p)
-#define CF_FD_CLR(n, p)     FD_CLR(n, p)
-#define CF_FD_ISSET(n, p)   FD_ISSET(n, p)
-#define CF_FD_ZERO(p)       FD_ZERO(p)
-#define CF_FD_SETSIZE       FD_SETSIZE
-
+/**
+ * fd_set
+ */
 typedef struct fd_set cf_fdset_t;
 
-cf_errno_t cf_select(int maxfdp, cf_fdset_t* readfds, cf_fdset_t* writefds, cf_fdset_t* errorfds, 
-    struct timeval* timeout);
+typedef struct timeval timeval;
 
+CF_DECLARE(cf_void_t) cf_fd_zero(cf_fdset_t* fdset);
+CF_DECLARE(cf_void_t) cf_fd_set(cf_int_t fd, cf_fdset_t* fdset);
+CF_DECLARE(cf_void_t) cf_fd_clr(cf_int_t fd, cf_fdset_t* fdset);
+CF_DECLARE(cf_void_t) cf_fd_isset(cf_int_t fd, cf_fdset_t* fdset);
+CF_DECLARE(cf_errno_t) cf_select(int maxfdp, cf_fdset_t* readfds, cf_fdset_t* writefds, cf_fdset_t* errorfds, 
+    struct timeval* timeout);
 
 #endif /* __CF_SELECT_H__ */
--- src/common/select.c
+++ src/common/select.c
@@ -2,8 +2,33 @@
 #include <cf/err.h>
 #include <sys/time.h>
 
-cf_errno_t cf_select(int maxfdp, cf_fdset_t* readfds, cf_fdset_t* writefds, cf_fdset_t* errorfds,
+#ifdef CF_OS_WIN
+
+#else
+#include <sys/select.h>
+#endif
+
+
+const cf_size_t CF_FD_SETSIZE = FD_SETSIZE;
+
+CF_DECLARE(cf_void_t) cf_fd_zero(cf_fdset_t* fdset) {
+    FD_ZERO(fdset);
+}
+
+CF_DECLARE(cf_void_t) cf_fd_set(cf_int_t fd, cf_fdset_t* fdset) {
+    FD_SET(fd, fdset);
+}
+
+CF_DECLARE(cf_void_t) cf_fd_clr(cf_int_t fd, cf_fdset_t* fdset) {
+    FD_CLR(fd, fdset);
+}
+
+CF_DECLARE(cf_void_t) cf_fd_isset(cf_int_t fd, cf_fdset_t* fdset) {
+    FD_ISSET(fd, fdset);
+}
+
+CF_DECLARE(cf_errno_t) cf_select(int maxfdp, cf_fdset_t* readfds, cf_fdset_t* writefds, cf_fdset_t* errorfds,
                      struct timeval* timeout) {
-    
-    return CF_OK;
+    if(!readfds || !writefds || !errorfds || !timeout) return CF_EPARAM;
+    return select(maxfdp, readfds, writefds, errorfds, timeout) > 0 ? CF_OK : CF_NOK;
 }
