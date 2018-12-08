c66967931b8f322ad1d20a274e94a4d6ab088aa7
tkorays <tkorays@hotmail.com>
2018-12-08 21:38:39 +0800

    env

--- src/common/env.c
+++ src/common/env.c
@@ -1,15 +1,47 @@
-#include "cf/env.h"
+#include <cf/env.h>
+#include <cf/err.h>
+#include <cf/str.h>
 
+#ifdef CF_OS_WIN
+    #include <windows.h>
+#else 
+    #include <unistd.h>
+    #include <stdlib.h>
+#endif
 
 cf_errno_t cf_env_get(const cf_char_t* name, cf_char_t* value, cf_size_t size) {
+    const char* v = 0;
+    if(!name || !value) return CF_EPARAM;
+#ifdef CF_OS_WIN
+
+#else 
+    v = getenv(name);
+    if(!v) return CF_ENOK;
+
+    if(!cf_strcpy_s(value, size, v)) return CF_ENOK;
+#endif
+
     return CF_EOK;
 }
 
 cf_errno_t cf_env_set(const cf_char_t* name, const cf_char_t* value) {
+    if(!name || !value) return CF_EPARAM;
+#ifdef CF_OS_WIN
+
+#else 
+    if(0 > setenv(name, value, 1)) return CF_ENOK;
+#endif
+
     return CF_EOK;
 }
 
 cf_errno_t cf_env_delete(const cf_char_t* name) {
+    if(!name) return CF_EPARAM;
+#ifdef CF_OS_WIN
+
+#else 
+    unsetenv(name);
+#endif
     return CF_EOK;
 }
 
