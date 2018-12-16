a7a1e50b79ea07e331d707a5d4b4620c88f8167c
tkorays <tkorays@hotmail.com>
2018-12-16 23:06:19 +0800

    window

--- src/common/system.c
+++ src/common/system.c
@@ -10,6 +10,9 @@
 #endif
 
 CF_DECLARE(cf_cpu_type_t) cf_sys_cpu_type() {
+#ifdef CF_OS_WIN
+    return CF_CPU_TYPE_OTHER;
+#else 
     cf_int_t cputype;
     size_t size;
     sysctlbyname("hw.cputype", &cputype, &size, NULL, 0);
@@ -19,45 +22,71 @@ CF_DECLARE(cf_cpu_type_t) cf_sys_cpu_type() {
     case CPU_TYPE_POWERPC: return CF_CPU_TYPE_POWERPC;
     default: return CF_CPU_TYPE_OTHER;
     }
+#endif
 }
 CF_DECLARE(cf_cpu_arch_t) cf_sys_cpu_arch() {
     return 0;
 }
 
 CF_DECLARE(cf_int_t) cf_sys_cpu_core_count() {
+#ifdef CF_OS_WIN
+    return 0;
+#else
     int count = 0;
     size_t size = sizeof(int);
     sysctlbyname("machdep.cpu.core_count", &count, &size, NULL, 0);
     return count;
+#endif
 }
 
 CF_DECLARE(cf_int_t) cf_sys_cpu_thread_count() {
+#ifdef CF_OS_WIN
+    return 0;
+#else 
     int count = 0;
     size_t size = sizeof(int);
     sysctlbyname("machdep.cpu.thread_count", &count, &size, NULL, 0);
     return count;
+#endif
 }
 
 CF_DECLARE(cf_int_t) cf_sys_cpu_freq_mhz() {
+#ifdef CF_OS_WIN
+    return 0;
+#else 
     cf_uint64_t freq = 0;
     size_t size = sizeof(cf_uint64_t);
     sysctlbyname("hw.cpufrequency", &freq, &size, NULL, 0);
     return (cf_int_t)(freq /1000000);
+#endif
 }
 
 CF_DECLARE(cf_void_t) cf_sys_cpu_brand(cf_char_t* buf, cf_size_t size) {
+#ifdef CF_OS_WIN
+    return ;
+
+#else 
     if(!buf) return ;
     sysctlbyname("machdep.cpu.brand_string", buf, &size, NULL, 0);
+#endif
 }
 
 CF_DECLARE(cf_int_t) cf_sys_mem_size_m() {
+#ifdef CF_OS_WIN
+    return 0;
+#else 
     cf_uint64_t mem;
     size_t size = sizeof(cf_uint64_t);
     sysctlbyname("hw.memsize", &mem, &size, NULL, 0);
     return (cf_int_t)(mem >> 20);
+#endif
 }
 
 CF_DECLARE(cf_void_t) cf_sys_hostname(cf_char_t* buf, cf_size_t size) {
+#ifdef CF_OS_WIN
+    return ;
+#else 
     if(!buf) return;
     sysctlbyname("kern.hostname", buf, &size, NULL, 0);
+#endif
 }
