38ab0f111630fcaa8872d25201c0f83a9d077877
tkorays <tkorays@hotmail.com>
2019-11-21 23:53:32 +0800

    git CI error: system

--- src/core/system.c
+++ src/core/system.c
@@ -1,18 +1,15 @@
 #include <cf/system.h>
 #ifdef CF_OS_WIN
 
-#elif CF_OS_MAC
-
+#else
 #include <sys/sysctl.h>
 #include <mach/machine.h>
-#else 
-
 #endif
 
 CF_DECLARE(cf_cpu_type_t) cf_sys_cpu_type() {
 #ifdef CF_OS_WIN
     return CF_CPU_TYPE_OTHER;
-#else 
+#elif defined(CF_OS_MAC)
     cf_int_t cputype;
     size_t size;
     sysctlbyname("hw.cputype", &cputype, &size, NULL, 0);
@@ -22,6 +19,8 @@ CF_DECLARE(cf_cpu_type_t) cf_sys_cpu_type() {
     case CPU_TYPE_POWERPC: return CF_CPU_TYPE_POWERPC;
     default: return CF_CPU_TYPE_OTHER;
     }
+#else
+    return CF_CPU_TYPE_OTHER;
 #endif
 }
 CF_DECLARE(cf_cpu_arch_t) cf_sys_cpu_arch() {
@@ -42,7 +41,7 @@ CF_DECLARE(cf_int_t) cf_sys_cpu_core_count() {
 CF_DECLARE(cf_int_t) cf_sys_cpu_thread_count() {
 #ifdef CF_OS_WIN
     return 0;
-#else 
+#else
     int count = 0;
     size_t size = sizeof(int);
     sysctlbyname("machdep.cpu.thread_count", &count, &size, NULL, 0);
@@ -53,7 +52,7 @@ CF_DECLARE(cf_int_t) cf_sys_cpu_thread_count() {
 CF_DECLARE(cf_int_t) cf_sys_cpu_freq_mhz() {
 #ifdef CF_OS_WIN
     return 0;
-#else 
+#else
     cf_uint64_t freq = 0;
     size_t size = sizeof(cf_uint64_t);
     sysctlbyname("hw.cpufrequency", &freq, &size, NULL, 0);
@@ -65,7 +64,7 @@ CF_DECLARE(cf_void_t) cf_sys_cpu_brand(cf_char_t* buf, cf_size_t size) {
 #ifdef CF_OS_WIN
     return ;
 
-#else 
+#else
     if(!buf) return ;
     sysctlbyname("machdep.cpu.brand_string", buf, &size, NULL, 0);
 #endif
