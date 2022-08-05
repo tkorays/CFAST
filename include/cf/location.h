fd39d2815e8f3f9612b0ca0d65b8be303e44efb2
tkorays <tkorays@hotmail.com>
2022-08-05 23:10:19 +0800

    compile error

--- include/cf/location.h
+++ include/cf/location.h
@@ -6,9 +6,9 @@
 CF_DECLS_BEGIN
 
 typedef struct {
-    char*   file_name;      //< filename of this location
-    char*   function_name;  //< function name of this location
-    int     line_number;    //< line number of this location
+    const char* file_name;      //< filename of this location
+    const char* function_name;  //< function name of this location
+    int         line_number;    //< line number of this location
 } cf_location_t;
 
 /**
@@ -18,6 +18,9 @@ typedef struct {
 #define cf_location_define(location) cf_location_t location;\
     do { (location).file_name = __FILE__; \
         (location).function_name = __FUNCTION__; (location).line_number = __LINE__; } while(0)
+
+#define cf_location_set(self) do { (self)->file_name = __FILE__; \
+        (self)->function_name = __FUNCTION__; (self)->line_number = __LINE__; } while(0)
 
 CF_DECLS_END
 
