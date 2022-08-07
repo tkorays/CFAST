c4cc497ee9de4deddc033038eab3385a73b7c6d0
tkorays <tkorays@hotmail.com>
2022-08-07 10:28:41 +0800

    vector impl

--- include/cf/vector.h
+++ include/cf/vector.h
@@ -1,99 +1,109 @@
-#ifndef __TEMPLE_RTC_BASE_VECTOR_H__
-#define __TEMPLE_RTC_BASE_VECTOR_H__
+/****************************************************
+ * Copyright (c) 2018, tkorays <tkorays@hotmail.com>
+ * All rights reserved.
+ * 
+ * @file    vector.h
+ * @brief   vector implimentation.
+ * @version 1.0
+ * @author  tkorays<tkorays@hotmail.com>
+ * @date    2022-8-7
+ ****************************************************/
+#ifndef __CFAST_VECTOR_H__
+#define __CFAST_VECTOR_H__
 
-#include "base/types.h"
+#include "cf/types.h"
 
-typedef struct rtc_vector {
-    void*       _buffer;
-    size_t      _capacity;
-    size_t      _elm_size;
-    size_t      _elm_count;
-    void*       _front;
-    void*       _back;
-    rtc_bool    _self_managed;
-} rtc_vector;
+typedef struct {
+    void*           buffer;
+    cf_size_t       capacity;
+    cf_size_t       elm_size;
+    cf_size_t       elm_count;
+    void*           front;
+    void*           back;
+} cf_vector_t;
 
-rtc_vector* rtc_vector_new(rtc_vector* vec, size_t elm_size);
-void rtc_vector_free(rtc_vector* vec);
-void rtc_vector_reserve(rtc_vector* vec, size_t cnt);
-rtc_bool rtc_vector_push_back(rtc_vector* vec, void* data, size_t size);
-rtc_bool rtc_vector_push_front(rtc_vector* vec, void* data, size_t size);
-rtc_bool rtc_vector_pop_back(rtc_vector* vec, void* data, size_t size);
-rtc_bool rtc_vector_pop_front(rtc_vector* vec, void* data, size_t size);
-rtc_bool rtc_vector_back(rtc_vector* vec, void* data, size_t size);
-rtc_bool rtc_vector_front(rtc_vector* vec, void* data, size_t size);
-rtc_bool rtc_vector_at(rtc_vector* vec, int idx, void* data, size_t size);
-rtc_bool rtc_vector_resize(rtc_vector* vec, size_t new_size, void* data);
-void rtc_vector_clear(rtc_vector* vec);
-static inline size_t rtc_vector_size(rtc_vector* vec) { return vec->_elm_count; }
-static inline rtc_bool rtc_vector_is_empty(rtc_vector* vec) { return vec->_elm_count == 0; }
 
-#define RTC_VECTOR_DEFINE_PUSH_BACK_FUNC(t)                             \
-static inline rtc_bool rtc_vector_push_back_##t(rtc_vector* vec, t v) { \
-    return rtc_vector_push_back(vec, &v, sizeof(t));                    \
+cf_bool_t cf_vector_init(cf_vector_t* self, size_t elm_size);
+void cf_vector_deinit(cf_vector_t* self);
+void cf_vector_reserve(cf_vector_t* vec, size_t cnt);
+cf_bool_t cf_vector_push_back(cf_vector_t* vec, void* data, size_t size);
+cf_bool_t cf_vector_push_front(cf_vector_t* vec, void* data, size_t size);
+cf_bool_t cf_vector_pop_back(cf_vector_t* vec, void* data, size_t size);
+cf_bool_t cf_vector_pop_front(cf_vector_t* vec, void* data, size_t size);
+cf_bool_t cf_vector_back(cf_vector_t* vec, void* data, size_t size);
+cf_bool_t cf_vector_front(cf_vector_t* vec, void* data, size_t size);
+cf_bool_t cf_vector_at(cf_vector_t* vec, int idx, void* data, size_t size);
+cf_bool_t cf_vector_resize(cf_vector_t* vec, size_t new_size, void* data);
+void cf_vector_clear(cf_vector_t* vec);
+static inline cf_size_t cf_vector_size(cf_vector_t* vec) { return vec->elm_count; }
+static inline cf_bool_t cf_vector_is_empty(cf_vector_t* vec) { return vec->elm_count == 0; }
+
+#define CF_VECTOR_DEFINE_PUSH_BACK_FUNC(t)                              \
+static inline cf_bool_t cf_vector_push_back_##t(cf_vector_t* vec, t v) { \
+    return cf_vector_push_back(vec, &v, sizeof(t));                    \
 }
 
-#define RTC_VECTOR_DEFINE_PUSH_FRONT_FUNC(t)                            \
-static inline rtc_bool rtc_vector_push_front_##t(rtc_vector* vec, t v) {\
-    return rtc_vector_push_front(vec, &v, sizeof(t));                   \
+#define CF_VECTOR_DEFINE_PUSH_FRONT_FUNC(t)                             \
+static inline cf_bool_t cf_vector_push_front_##t(cf_vector_t* vec, t v) {\
+    return cf_vector_push_front(vec, &v, sizeof(t));                   \
 }
 
-#define RTC_VECTOR_DEFINE_POP_BACK_FUNC(t)                              \
-static inline t rtc_vector_pop_back_##t(rtc_vector* vec) {              \
+#define CF_VECTOR_DEFINE_POP_BACK_FUNC(t)                               \
+static inline t cf_vector_pop_back_##t(cf_vector_t* vec) {              \
     t v;                                                                \
-    rtc_vector_pop_back(vec, &v, sizeof(v));                            \
+    cf_vector_pop_back(vec, &v, sizeof(v));                            \
     return v;                                                           \
 }
 
-#define RTC_VECTOR_DEFINE_POP_FRONT_FUNC(t)                             \
-static inline t rtc_vector_pop_front_##t(rtc_vector* vec) {             \
+#define CF_VECTOR_DEFINE_POP_FRONT_FUNC(t)                              \
+static inline t cf_vector_pop_front_##t(cf_vector_t* vec) {             \
     t v;                                                                \
-    rtc_vector_pop_front(vec, &v, sizeof(t));                           \
+    cf_vector_pop_front(vec, &v, sizeof(t));                           \
     return v;                                                           \
 }
 
-RTC_VECTOR_DEFINE_PUSH_BACK_FUNC(char)
-RTC_VECTOR_DEFINE_PUSH_BACK_FUNC(int)
-RTC_VECTOR_DEFINE_PUSH_BACK_FUNC(int8_t)
-RTC_VECTOR_DEFINE_PUSH_BACK_FUNC(uint8_t)
-RTC_VECTOR_DEFINE_PUSH_BACK_FUNC(int16_t)
-RTC_VECTOR_DEFINE_PUSH_BACK_FUNC(uint16_t)
-RTC_VECTOR_DEFINE_PUSH_BACK_FUNC(int32_t)
-RTC_VECTOR_DEFINE_PUSH_BACK_FUNC(uint32_t)
-RTC_VECTOR_DEFINE_PUSH_BACK_FUNC(int64_t)
-RTC_VECTOR_DEFINE_PUSH_BACK_FUNC(uint64_t)
+CF_VECTOR_DEFINE_PUSH_BACK_FUNC(char)
+CF_VECTOR_DEFINE_PUSH_BACK_FUNC(int)
+CF_VECTOR_DEFINE_PUSH_BACK_FUNC(int8_t)
+CF_VECTOR_DEFINE_PUSH_BACK_FUNC(uint8_t)
+CF_VECTOR_DEFINE_PUSH_BACK_FUNC(int16_t)
+CF_VECTOR_DEFINE_PUSH_BACK_FUNC(uint16_t)
+CF_VECTOR_DEFINE_PUSH_BACK_FUNC(int32_t)
+CF_VECTOR_DEFINE_PUSH_BACK_FUNC(uint32_t)
+CF_VECTOR_DEFINE_PUSH_BACK_FUNC(int64_t)
+CF_VECTOR_DEFINE_PUSH_BACK_FUNC(uint64_t)
 
-RTC_VECTOR_DEFINE_PUSH_FRONT_FUNC(char)
-RTC_VECTOR_DEFINE_PUSH_FRONT_FUNC(int)
-RTC_VECTOR_DEFINE_PUSH_FRONT_FUNC(int8_t)
-RTC_VECTOR_DEFINE_PUSH_FRONT_FUNC(uint8_t)
-RTC_VECTOR_DEFINE_PUSH_FRONT_FUNC(int16_t)
-RTC_VECTOR_DEFINE_PUSH_FRONT_FUNC(uint16_t)
-RTC_VECTOR_DEFINE_PUSH_FRONT_FUNC(int32_t)
-RTC_VECTOR_DEFINE_PUSH_FRONT_FUNC(uint32_t)
-RTC_VECTOR_DEFINE_PUSH_FRONT_FUNC(int64_t)
-RTC_VECTOR_DEFINE_PUSH_FRONT_FUNC(uint64_t)
+CF_VECTOR_DEFINE_PUSH_FRONT_FUNC(char)
+CF_VECTOR_DEFINE_PUSH_FRONT_FUNC(int)
+CF_VECTOR_DEFINE_PUSH_FRONT_FUNC(int8_t)
+CF_VECTOR_DEFINE_PUSH_FRONT_FUNC(uint8_t)
+CF_VECTOR_DEFINE_PUSH_FRONT_FUNC(int16_t)
+CF_VECTOR_DEFINE_PUSH_FRONT_FUNC(uint16_t)
+CF_VECTOR_DEFINE_PUSH_FRONT_FUNC(int32_t)
+CF_VECTOR_DEFINE_PUSH_FRONT_FUNC(uint32_t)
+CF_VECTOR_DEFINE_PUSH_FRONT_FUNC(int64_t)
+CF_VECTOR_DEFINE_PUSH_FRONT_FUNC(uint64_t)
 
-RTC_VECTOR_DEFINE_POP_BACK_FUNC(char)
-RTC_VECTOR_DEFINE_POP_BACK_FUNC(int)
-RTC_VECTOR_DEFINE_POP_BACK_FUNC(int8_t)
-RTC_VECTOR_DEFINE_POP_BACK_FUNC(uint8_t)
-RTC_VECTOR_DEFINE_POP_BACK_FUNC(int16_t)
-RTC_VECTOR_DEFINE_POP_BACK_FUNC(uint16_t)
-RTC_VECTOR_DEFINE_POP_BACK_FUNC(int32_t)
-RTC_VECTOR_DEFINE_POP_BACK_FUNC(uint32_t)
-RTC_VECTOR_DEFINE_POP_BACK_FUNC(int64_t)
-RTC_VECTOR_DEFINE_POP_BACK_FUNC(uint64_t)
+CF_VECTOR_DEFINE_POP_BACK_FUNC(char)
+CF_VECTOR_DEFINE_POP_BACK_FUNC(int)
+CF_VECTOR_DEFINE_POP_BACK_FUNC(int8_t)
+CF_VECTOR_DEFINE_POP_BACK_FUNC(uint8_t)
+CF_VECTOR_DEFINE_POP_BACK_FUNC(int16_t)
+CF_VECTOR_DEFINE_POP_BACK_FUNC(uint16_t)
+CF_VECTOR_DEFINE_POP_BACK_FUNC(int32_t)
+CF_VECTOR_DEFINE_POP_BACK_FUNC(uint32_t)
+CF_VECTOR_DEFINE_POP_BACK_FUNC(int64_t)
+CF_VECTOR_DEFINE_POP_BACK_FUNC(uint64_t)
 
-RTC_VECTOR_DEFINE_POP_FRONT_FUNC(char)
-RTC_VECTOR_DEFINE_POP_FRONT_FUNC(int)
-RTC_VECTOR_DEFINE_POP_FRONT_FUNC(int8_t)
-RTC_VECTOR_DEFINE_POP_FRONT_FUNC(uint8_t)
-RTC_VECTOR_DEFINE_POP_FRONT_FUNC(int16_t)
-RTC_VECTOR_DEFINE_POP_FRONT_FUNC(uint16_t)
-RTC_VECTOR_DEFINE_POP_FRONT_FUNC(int32_t)
-RTC_VECTOR_DEFINE_POP_FRONT_FUNC(uint32_t)
-RTC_VECTOR_DEFINE_POP_FRONT_FUNC(int64_t)
-RTC_VECTOR_DEFINE_POP_FRONT_FUNC(uint64_t)
+CF_VECTOR_DEFINE_POP_FRONT_FUNC(char)
+CF_VECTOR_DEFINE_POP_FRONT_FUNC(int)
+CF_VECTOR_DEFINE_POP_FRONT_FUNC(int8_t)
+CF_VECTOR_DEFINE_POP_FRONT_FUNC(uint8_t)
+CF_VECTOR_DEFINE_POP_FRONT_FUNC(int16_t)
+CF_VECTOR_DEFINE_POP_FRONT_FUNC(uint16_t)
+CF_VECTOR_DEFINE_POP_FRONT_FUNC(int32_t)
+CF_VECTOR_DEFINE_POP_FRONT_FUNC(uint32_t)
+CF_VECTOR_DEFINE_POP_FRONT_FUNC(int64_t)
+CF_VECTOR_DEFINE_POP_FRONT_FUNC(uint64_t)
 
-#endif // __TEMPLE_RTC_BASE_VECTOR_H__
+#endif /* __CFAST_VECTOR_H__ */
--- src/core/vector.c
+++ src/core/vector.c
@@ -1,188 +1,183 @@
-#include "base/vector.h"
-#include "base/memory.h"
-#include "base/check.h"
+#include "cf/vector.h"
+#include "cf/memory.h"
 
-#define RTC_VECTOR_BUF_DEF_BYTES 512
-#define RTC_VECTOR_BUF_DEF_COUNT 128
 
-static inline rtc_bool _rtc_vectory_copy_buffer(rtc_vector* vec, void* new_buffer, size_t new_capacity) {
+#define cf_vector_BUF_DEF_BYTES 512
+#define cf_vector_BUF_DEF_COUNT 128
+
+static inline cf_bool_t _cf_vectory_copy_buffer(cf_vector_t* vec, void* new_buffer, size_t new_capacity) {
     uint64_t tmp1        = 0;
     uint64_t tmp2        = 0;
-    if (vec->_elm_count == 0) {
-        rtc_free(vec->_buffer);
-        vec->_capacity  = new_capacity;
-        vec->_buffer    = new_buffer;
-        vec->_front     = new_buffer;
-        vec->_back      = new_buffer;
+    if (vec->elm_count == 0) {
+        rtc_free(vec->buffer);
+        vec->capacity  = new_capacity;
+        vec->buffer    = new_buffer;
+        vec->front     = new_buffer;
+        vec->back      = new_buffer;
     } else {
-        if ((uint64_t)(vec->_front) < (uint64_t)(vec->_back)) {
-            memcpy(new_buffer, vec->_front, vec->_elm_count * vec->_elm_size);
+        if ((uint64_t)(vec->front) < (uint64_t)(vec->back)) {
+            memcpy(new_buffer, vec->front, vec->elm_count * vec->elm_size);
         } else {
-            tmp1 = vec->_capacity * vec->_elm_size - ((uint64_t)vec->_front - (uint64_t)vec->_buffer + vec->_elm_size);
-            memcpy(new_buffer, vec->_front, tmp1);
-            tmp2 = vec->_elm_count * vec->_elm_size - tmp1;
+            tmp1 = vec->capacity * vec->elm_size - ((uint64_t)vec->front - (uint64_t)vec->buffer + vec->elm_size);
+            memcpy(new_buffer, vec->front, tmp1);
+            tmp2 = vec->elm_count * vec->elm_size - tmp1;
             if (tmp2 > 0) {
-                memcpy((uint8_t*)new_buffer + tmp1, vec->_buffer, tmp2);
+                memcpy((uint8_t*)new_buffer + tmp1, vec->buffer, tmp2);
             }
         }  
-        rtc_free(vec->_buffer);
-        vec->_capacity  = new_capacity;
-        vec->_buffer    = new_buffer;
-        vec->_front     = new_buffer;
-        vec->_back      = (void*)((int8_t*)new_buffer + vec->_elm_count * vec->_elm_size);
+        rtc_free(vec->buffer);
+        vec->capacity  = new_capacity;
+        vec->buffer    = new_buffer;
+        vec->front     = new_buffer;
+        vec->back      = (void*)((int8_t*)new_buffer + vec->elm_count * vec->elm_size);
     }
 
-    return RTC_TRUE;
+    return CF_TRUE;
 }
 
-static inline void* _rtc_vector_next_of(void* buffer, size_t elm_size, size_t capacity, void* current) {
+static inline void* _cf_vector_next_of(void* buffer, size_t elm_size, size_t capacity, void* current) {
     int offset;
     offset = (uint64_t)current - (uint64_t)buffer;
     offset = offset == (capacity - 1) * elm_size ? 0 : offset + elm_size;
-    return RTC_CAST(void*, (uint8_t*)buffer + offset);
+    return CF_CAST(void*, (uint8_t*)buffer + offset);
 }
 
-static inline void* _rtc_vector_prev_of(void* buffer, size_t elm_size, size_t capacity, void* current) {
+static inline void* _cf_vector_prev_of(void* buffer, size_t elm_size, size_t capacity, void* current) {
     int offset;
     offset = (uint64_t)current - (uint64_t)buffer;
     offset = offset == 0 ? (capacity - 1) * elm_size : offset - elm_size;
-    return RTC_CAST(void*, (uint8_t*)buffer + offset);
+    return CF_CAST(void*, (uint8_t*)buffer + offset);
 }
 
-rtc_vector* rtc_vector_new(rtc_vector* vec, size_t elm_size) {
-    if (elm_size == 0) return RTC_NULL;
-
-    if (vec) {
-        vec->_self_managed = RTC_FALSE;
-    } else {
-        vec = RTC_CAST(rtc_vector*, rtc_malloc(sizeof(rtc_vector)));
-        vec->_self_managed = RTC_TRUE;
+cf_bool_t cf_vector_init(cf_vector_t* self, size_t elm_size) {
+    self->capacity  = CF_MIN2(cf_vector_BUF_DEF_BYTES, cf_vector_BUF_DEF_COUNT * elm_size) / elm_size;
+    self->elm_size  = elm_size;
+    self->buffer    = cf_malloc(self->capacity * elm_size);
+    if (!self->buffer) {
+        return CF_NULL;
     }
-
-
-    vec->_capacity  = RTC_MIN(RTC_VECTOR_BUF_DEF_BYTES, RTC_VECTOR_BUF_DEF_COUNT * elm_size) / elm_size;
-    vec->_elm_size  = elm_size;
-    vec->_buffer    = rtc_malloc(vec->_capacity * elm_size);
-    if (!vec->_buffer) {
-        if (vec->_self_managed) rtc_free(vec);
-        return RTC_NULL;
-    }
-    vec->_elm_count = 0;
-    vec->_front     = vec->_buffer;
-    vec->_back      = vec->_buffer;
-    return vec;
+    self->elm_count = 0;
+    self->front     = self->buffer;
+    self->back      = self->buffer;
+    return self;
 }
 
-void rtc_vector_free(rtc_vector* vec) {
-    if (vec->_self_managed) {
-        rtc_free(vec);
+void cf_vector_deinit(cf_vector_t* self) {
+    if (self->buffer) {
+        cf_free(self->buffer);
     }
+    self->buffer    = CF_NULL_PTR;
+    self->capacity  = 0;
+    self->elm_size  = 0;
+    self->elm_count = 0;
+    self->front     = 0;
+    self->back      = 0;
 }
 
-void rtc_vector_reserve(rtc_vector* vec, size_t cnt) {
-    void* new_buffer    = RTC_NULL;
-    if (cnt <= vec->_capacity) return;
-    new_buffer = rtc_malloc(cnt * vec->_elm_size);
+void cf_vector_reserve(cf_vector_t* vec, size_t cnt) {
+    void* new_buffer    = CF_NULL;
+    if (cnt <= vec->capacity) return;
+    new_buffer = cf_malloc(cnt * vec->elm_size);
     if (!new_buffer) {
         return;
     }
 
-    _rtc_vectory_copy_buffer(vec, new_buffer, cnt);
+    _cf_vectory_copy_buffer(vec, new_buffer, cnt);
 }
 
-rtc_bool rtc_vector_push_back(rtc_vector* vec, void* data, size_t size) {
-    RTC_CHECK_GE(size, vec->_elm_size);
-    if (!data) return RTC_FALSE;
+cf_bool_t cf_vector_push_back(cf_vector_t* vec, void* data, size_t size) {
+    CF_CHECK_GE(size, vec->elm_size);
+    if (!data) return CF_FALSE;
 
-    if (vec->_capacity <= vec->_elm_count) {
-        rtc_vector_reserve(vec, vec->_capacity * 2);
-        if (vec->_capacity <= vec->_elm_count) {
-            return RTC_FALSE;
+    if (vec->capacity <= vec->elm_count) {
+        cf_vector_reserve(vec, vec->capacity * 2);
+        if (vec->capacity <= vec->elm_count) {
+            return CF_FALSE;
         }
     }
 
-    vec->_back = vec->_elm_count == 0 ? vec->_buffer :
-        _rtc_vector_next_of(vec->_buffer, vec->_elm_size, vec->_capacity, vec->_back);
-    memcpy(vec->_back, data, size);
-    vec->_elm_count++;
-    return RTC_TRUE;
+    vec->back = vec->elm_count == 0 ? vec->buffer :
+        _cf_vector_next_of(vec->buffer, vec->elm_size, vec->capacity, vec->back);
+    memcpy(vec->back, data, size);
+    vec->elm_count++;
+    return CF_TRUE;
 }
 
-rtc_bool rtc_vector_push_front(rtc_vector* vec, void* data, size_t size) {
-    RTC_CHECK_GE(size, vec->_elm_size);
-    if (!data) return RTC_FALSE;
+cf_bool_t cf_vector_push_front(cf_vector_t* vec, void* data, size_t size) {
+    CF_CHECK_GE(size, vec->elm_size);
+    if (!data) return CF_FALSE;
 
-    if (vec->_capacity <= vec->_elm_count) {
-        rtc_vector_reserve(vec, vec->_capacity * 2);
-        if (vec->_capacity <= vec->_elm_count) {
-            return RTC_FALSE;
+    if (vec->capacity <= vec->elm_count) {
+        cf_vector_reserve(vec, vec->capacity * 2);
+        if (vec->capacity <= vec->elm_count) {
+            return CF_FALSE;
         }
     }
-    vec->_front = vec->_elm_count == 0 ? vec->_buffer :
-        _rtc_vector_prev_of(vec->_buffer, vec->_elm_size, vec->_capacity, vec->_front);
-    memcpy(vec->_front, data, size);
-    vec->_elm_count++;
-    return RTC_TRUE;
+    vec->front = vec->elm_count == 0 ? vec->buffer :
+        _cf_vector_prev_of(vec->buffer, vec->elm_size, vec->capacity, vec->front);
+    memcpy(vec->front, data, size);
+    vec->elm_count++;
+    return CF_TRUE;
 }
 
-rtc_bool rtc_vector_pop_back(rtc_vector* vec, void* data, size_t size) {
-    RTC_CHECK_GE(size, vec->_elm_size);
-    if (!data) return RTC_FALSE;
+cf_bool_t cf_vector_pop_back(cf_vector_t* vec, void* data, size_t size) {
+    CF_CHECK_GE(size, vec->elm_size);
+    if (!data) return CF_FALSE;
 
-    if (rtc_vector_is_empty(vec)) return RTC_FALSE;
-    memcpy(data, vec->_back, size);
+    if (cf_vector_is_empty(vec)) return CF_FALSE;
+    memcpy(data, vec->back, size);
 
-    vec->_back = _rtc_vector_prev_of(vec->_buffer, vec->_elm_size, vec->_capacity, vec->_back);
-    vec->_elm_count--;
-    return RTC_TRUE;
+    vec->back = _cf_vector_prev_of(vec->buffer, vec->elm_size, vec->capacity, vec->back);
+    vec->elm_count--;
+    return CF_TRUE;
 }
 
-rtc_bool rtc_vector_pop_front(rtc_vector* vec, void* data, size_t size) {
-    RTC_CHECK_GE(size, vec->_elm_size);
-    if (!data) return RTC_FALSE;
+cf_bool_t cf_vector_pop_front(cf_vector_t* vec, void* data, size_t size) {
+    CF_CHECK_GE(size, vec->elm_size);
+    if (!data) return CF_FALSE;
 
-    if (rtc_vector_is_empty(vec)) return RTC_FALSE;
-    memcpy(data, vec->_front, size);
-    vec->_front = _rtc_vector_next_of(vec->_buffer, vec->_elm_count, vec->_capacity, vec->_front);
-    vec->_elm_count--;
-    return RTC_TRUE;
+    if (cf_vector_is_empty(vec)) return CF_FALSE;
+    memcpy(data, vec->front, size);
+    vec->front = _cf_vector_next_of(vec->buffer, vec->elm_count, vec->capacity, vec->front);
+    vec->elm_count--;
+    return CF_TRUE;
 }
 
-rtc_bool rtc_vector_back(rtc_vector* vec, void* data, size_t size) {
-    RTC_CHECK_GE(size, vec->_elm_size);
-    if (!data) return RTC_FALSE;
-    memcpy(data, vec->_back, size);
-    return RTC_TRUE;
+cf_bool_t cf_vector_back(cf_vector_t* vec, void* data, size_t size) {
+    CF_CHECK_GE(size, vec->elm_size);
+    if (!data) return CF_FALSE;
+    memcpy(data, vec->back, size);
+    return CF_TRUE;
 }
 
-rtc_bool rtc_vector_front(rtc_vector* vec, void* data, size_t size) {
-    RTC_CHECK_GE(size, vec->_elm_size);
-    if (!data) return RTC_FALSE;
-    memcpy(data, vec->_front, size);
-    return RTC_TRUE;
+cf_bool_t cf_vector_front(cf_vector_t* vec, void* data, size_t size) {
+    CF_CHECK_GE(size, vec->elm_size);
+    if (!data) return CF_FALSE;
+    memcpy(data, vec->front, size);
+    return CF_TRUE;
 }
 
-rtc_bool rtc_vector_at(rtc_vector* vec, int idx, void* data, size_t size) {
-    if (!data || size != vec->_elm_size) return RTC_FALSE;
-    memcpy(data, (uint8_t*)vec->_buffer, size);
-    return RTC_TRUE;
+cf_bool_t cf_vector_at(cf_vector_t* vec, int idx, void* data, size_t size) {
+    if (!data || size != vec->elm_size) return CF_FALSE;
+    memcpy(data, (uint8_t*)vec->buffer, size);
+    return CF_TRUE;
 }
 
-rtc_bool rtc_vector_resize(rtc_vector* vec, size_t new_size, void* data) {
-    if (new_size > vec->_capacity) {
-        rtc_vector_reserve(vec, new_size);
-        if (new_size > vec->_capacity) return RTC_FALSE;
+cf_bool_t cf_vector_resize(cf_vector_t* vec, size_t new_size, void* data) {
+    if (new_size > vec->capacity) {
+        cf_vector_reserve(vec, new_size);
+        if (new_size > vec->capacity) return CF_FALSE;
     }
 
-    while (vec->_elm_count < new_size) {
-        rtc_vector_push_back(vec, data, vec->_elm_size);
+    while (vec->elm_count < new_size) {
+        cf_vector_push_back(vec, data, vec->elm_size);
     }
-    return RTC_TRUE;
+    return CF_TRUE;
 }
 
-void rtc_vector_clear(rtc_vector* vec) {
-    vec->_elm_count = 0;
-    vec->_front     = vec->_buffer;
-    vec->_back      = vec->_buffer;
+void cf_vector_clear(cf_vector_t* vec) {
+    vec->elm_count = 0;
+    vec->front     = vec->buffer;
+    vec->back      = vec->buffer;
 }
 
