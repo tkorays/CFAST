fb6dc45f971f14ebdb27e925bfa0e3850ba22672
tkorays <tkorays@hotmail.com>
2023-04-01 22:49:07 +0800

    add comments

--- include/cf/mutex.h
+++ include/cf/mutex.h
@@ -27,20 +27,59 @@
 
 CF_DECLS_BEGIN
 #ifdef CF_OS_WIN
-typedef HANDLE cf_mutex_t;
-typedef SECURITY_ATTRIBUTES cf_mutex_attr_t;
+typedef HANDLE cf_mutex_t; /**< Windows mutex type */
+typedef SECURITY_ATTRIBUTES cf_mutex_attr_t; /**< Windows mutex attribute type */
 #else
-typedef pthread_mutex_t cf_mutex_t;
-typedef pthread_mutexattr_t cf_mutex_attr_t;
+typedef pthread_mutex_t cf_mutex_t; /**< POSIX mutex type */
+typedef pthread_mutexattr_t cf_mutex_attr_t; /**< POSIX mutex attribute type */
 #endif
 
+/**
+ * @defgroup CF_MUTEX Mutex
+ * @brief A system independent mutex
+ * @{
+ */
+
+/**
+ * @brief Initialize a mutex
+ * @param mutex Pointer to the mutex to initialize
+ * @param attr Pointer to the mutex attribute to use
+ * @return CF_EOK on success, otherwise an error code
+ */
 cf_errno_t cf_mutex_init(cf_mutex_t* mutex, const cf_mutex_attr_t* attr);
+
+/**
+ * @brief Destroy a mutex
+ * @param mutex Pointer to the mutex to destroy
+ * @return CF_EOK on success, otherwise an error code
+ */
 cf_errno_t cf_mutex_destroy(cf_mutex_t* mutex);
+
+/**
+ * @brief Lock a mutex
+ * @param mutex Pointer to the mutex to lock
+ * @return CF_EOK on success, otherwise an error code
+ */
 cf_errno_t cf_mutex_lock(cf_mutex_t* mutex);
+
+/**
+ * @brief Try to lock a mutex
+ * @param mutex Pointer to the mutex to try to lock
+ * @return CF_EOK on success, otherwise an error code
+ */
 cf_errno_t cf_mutex_trylock(cf_mutex_t* mutex);
+
+/**
+ * @brief Unlock a mutex
+ * @param mutex Pointer to the mutex to unlock
+ * @return CF_EOK on success, otherwise an error code
+ */
 cf_errno_t cf_mutex_unlock(cf_mutex_t* mutex);
 
+/** @} */
+
 CF_DECLS_END
+
 
 /** @} */
 
